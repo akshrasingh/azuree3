873023dd5b52e774f94265bf35daba8c694454b6
Akshra Singh <110145062+akshrasingh@users.noreply.github.com>
2023-07-26 22:05:19 +0530

    Add files via upload

---
+++ code/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABotWithMSI.sln -p:DeployOnBuild=true -p:PublishProfile=.\HEYBOOT-bot-8703-Web-Deploy.pubxml -p:Password=oPLKfiwtbY75jlJeBlxa54z1kpZGMSicQ7fRWex8xqj6nf6HbpC0sS0t2jHX
+
